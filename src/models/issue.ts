bf6e2853e70178ca16c773eae6e7a1823fcf7df6
Biruk Tassew <biruktassew21@gmail.com>
2023-04-16 23:26:21 +0300

    feat(backend): Add model for issue

---
+++ src/models/issue.ts
@@ -0,0 +1,73 @@
+import Joi from "joi";
+import { Schema, Document, model, Model } from "mongoose";
+
+export interface IIssue extends Document {
+    userId: Schema.Types.ObjectId
+    classId: Schema.Types.ObjectId
+    title: string
+    description: string
+    attachments: [Schema.Types.ObjectId]
+    tags: [string]
+    createdAt: Date
+    updatedAt: Date
+}
+
+
+const IssueSchema: Schema<IIssue> = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+    classId: {
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: [true, "Description is required."]
+    },
+    attachments: {
+        type: [Schema.Types.ObjectId],
+        ref: 'File',
+        default: null,
+        required: false
+    },
+    tags: {
+        type: [String],
+        default: []
+    },
+    },
+    {
+        timestamps: {
+            createdAt: 'createdAt',
+            updatedAt: 'updatedAt'
+        },
+        populate: {
+            path: 'attachements',
+            model: 'File'
+        },
+    })
+
+
+const Issue = model<IIssue>('Issue', IssueSchema)
+
+const objectId = Joi.object({
+    id: Joi.string().hex().length(24)
+  })
+  
+export const issueValidation = Joi.object<IIssue>({
+    userId: objectId.required(),
+    classId: objectId.required(),
+    title: Joi.string().required(),
+    description: Joi.string().required().messages({
+      'any.required': 'Description is required.',
+    }),
+    attachments: Joi.array().items(objectId),
+    tags: Joi.array().items(Joi.string()),
+})
+
+export default Issue;
